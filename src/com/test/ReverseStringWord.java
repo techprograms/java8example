f9023e60d1b0097a5759c06cb1153113c76212f9
techprograms <tech.gpr@gmail.com>
2023-01-26 11:19:39 +0530

    reversing words of a string

---
+++ src/com/test/ReverseStringWord.java
@@ -0,0 +1,23 @@
+package com.test;
+
+public class ReverseStringWord {
+
+	public static String reverseStringWords(String str) {
+		String words[] = str.split(" ");
+		String reverseWord = "";
+		for(String w : words) {
+			StringBuilder sb = new StringBuilder(w);
+			sb.reverse();
+			reverseWord += sb.toString() + " ";
+		}
+		return reverseWord;
+	}
+	
+	public static void main(String[] args) {
+		
+		String str = "This is Great India";
+		StringBuilder sb = new StringBuilder(str);
+		StringBuilder rev = sb.reverse();
+		System.out.println(rev);
+	}
+}
