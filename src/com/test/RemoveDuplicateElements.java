7fc71ff6d5be81f52bfe828a73f77c2691c10546
techprograms <tech.gpr@gmail.com>
2023-01-26 19:53:19 +0530

    removing duplicate elements

---
+++ src/com/test/RemoveDuplicateElements.java
@@ -0,0 +1,15 @@
+package com.test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+public class RemoveDuplicateElements {
+	public static void main(String[] args) {
+		List<Integer> intList = Arrays.asList(2, 3, 4, 3, 5, 7, 3, 5, 8, 1, 9, 6);		
+		List<Integer> unique = intList.stream()
+				.distinct()
+				.sorted()
+				.collect(Collectors.toList());
+		System.out.println("List of Unique Elements : "+unique);
+	}
+}
